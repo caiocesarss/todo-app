abf5f0729a6c92e402db91e35dbbd2263a6e5b11
Caio Siqueira <caiosiqueira@outlook.com>
2019-05-03 20:01:30 -0300

    ver erro

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,5 @@
 const port = 3003
+
 const bodyParser = require ('body-parser')
 const express = require('express')
 const server = express()
@@ -9,3 +10,5 @@ server.use(bodyParser.json())
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}`)
 })
+
+module.exports = server
